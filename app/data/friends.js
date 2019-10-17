b00e7b588b268514bf7d1eece22e85c58bbcdf46
Daniel <dhuerta1408@gmail.com>
2019-10-17 09:33:14 -0600

    add friends javascript file and began functionality

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,56 @@
 var friends = [
     {
-        // name
-        // imagelink
-        // scores[array of numbers]
-    }
+        name: "Spongebob Squarepants",
+        photo: "https://dcmetrotheaterarts.com/wp-content/uploads/2017/12/SpongeBob_SquarePantscopy0.png",
+        scores: [3, 5, 1, 3, 5, 2, 2, 1, 2, 1]
+    },
+    {
+        name: "Patrick Star",
+        photo: "https://imagez.tmz.com/image/e2/o/2012/09/21/e26eed3923e154a9b26680ab2d3033e1_md.jpg",
+        scores: [1, 2, 1, 1, 4, 1, 4, 1, 1, 5]
+    },
+    {
+        name: "Squidward Tentacles",
+        photo: "https://i.pinimg.com/originals/fc/20/d7/fc20d7ba75499df5113b2b3f94b87730.jpg",
+        scores: [1, 1, 5, 3, 1, 2, 5, 3, 3, 3]
+    },
+    {
+        name: "Sandy Cheeks",
+        photo: "https://topicimages.mrowl.com/large/gracie/spongebobsquar/thecharacters/sandycheeks_1.jpg",
+        scores: [5, 5, 2, 4, 4, 5, 1, 3, 4, 1]
+    },  
+    {
+        name: "Mr. Krabs",
+        photo: "https://www.lifewire.com/thmb/R83AtLxKyYRbEzaoscsXoM0gO3k=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/spongebob-squarepants-nickelodeon-032916-1276x850-5bfecf8246e0fb0026823b0a.jpg",
+        scores: [1, 1, 3, 4, 3, 2, 3, 5, 3, 2]
+    },
+    {
+        name: "Plankton",
+        photo: "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/121024981/original/973e1c3b779166ca18762fd7e7e77fe7a0b93949/say-anything-you-want-me-to-say-as-plankton-from-spongebob.jpg",
+        scores: [5, 2, 4, 5, 2, 4, 4, 4, 2, 1]
+    },
+    {
+        name: "Karen",
+        photo: "https://i.ytimg.com/vi/jFCfA0_ZL1s/hqdefault.jpg",
+        scores: [5, 1, 3, 3, 1, 3, 2, 3, 5, 3]
+    },
+    {
+        name: "Mrs. Puff",
+        photo: "https://i.pinimg.com/originals/2a/f2/8d/2af28d4479f70bf6b8ba9cea70c7fa07.jpg",
+        scores: [3, 1, 2, 3, 3, 3, 2, 2, 4, 3]
+    },
+    {
+        name: "Larry Lobster",
+        photo: "https://i.redd.it/ywqgpu6iv3931.jpg",
+        scores: [1, 3, 1, 4, 4, 1, 1, 3, 2, 1]
+    },
+    {
+        name: "Gary",
+        photo: "https://imagez.tmz.com/image/e2/o/2012/09/21/e26eed3923e154a9b26680ab2d3033e1_md.jpg",
+        scores: [3, 2, 3, 3, 4, 4, 2, 1, 5, 4]
+    },
+    
+    
 ]
 
 
