1eabd1ce6ddf02efe532fa87388e35cbf0a6c22f
Daniel <dhuerta1408@gmail.com>
2019-10-17 17:06:45 -0600

    add styling and functionality

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,34 @@
-// backend javascript
-
-// require from friends.js friends = require
-// compare user data to friends data
-// loop through friends and friend data
-// absolute value
-// post to modal res.json
-// make modal display before you populate the modal
+var friends = require("../data/friends");
 
 module.exports = function(app) {
-    console.log("Loaded API routes")
-}
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+  app.post("/api/friends", function(req, res) {
+    var info = req.body;
+    for (var i = 0; i < info.scores.length; i++) {
+      info.scores[i] = parseInt(info.scores[i]);
+    }
+  });
+
+  var friendIndex = 0;
+  var discrepancy = 40;
+
+  for (var i = 0; i < friends.length; i++) {
+    var totalDiff = 0;
+    for (var f = 0; f < friends[i].scores.length; f++) {
+      var difference = Math.abs(info.scores[f] - friends[i].scores[f]);
+      totalDiff += difference;
+    }
+
+    if (totalDiff < discrepancy) {
+      friendIndex = i;
+      discrepancy = difference;
+    }
+  }
+
+  friends.push(info);
+
+  res.json(friends[friendIndex]);
+};
